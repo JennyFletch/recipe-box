b09c1ca43de3a921d0fd8951db505b64752613f8
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-09-17 17:48:52 -0600

    Set up custom stylesheet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Recipe Box - All the convenience of a countertop recipe box, minus the crumbs and splats.</title>
+@section('content')
 
-    </head>
-    <body>
+    <h1>Recipe Box <span class="d-block font-size-xs">All the convenience of a countertop recipe box, minus the crumbs and splats.</span></h1>
 
-        <ul class="header-nav">
+    <ul class="header-nav">
 
-            @if (Route::has('login'))
-                
-                    @auth
-                        <li><a href="{{ url('/dashboard') }}">dashboard</a></li>
-                    @else
-                        <li><a href="{{ route('login') }}">login</a></li>
-                        @if (Route::has('register'))
-                            <li><a href="{{ route('register') }}">register</a></li>
-                        @endif
-                    @endauth
-                
-            @endif
+        <!-- @if (Route::has('login'))
+                @auth
+                    <li><a href="{{ url('/dashboard') }}">dashboard</a></li>
+                @else
+                    <li><a href="{{ route('login') }}">login</a></li>
+                    @if (Route::has('register'))
+                        <li><a href="{{ route('register') }}">register</a></li>
+                    @endif
+                @endauth
+        @endif -->
 
-        </ul>
+    </ul>
 
-        <h1>Recipe Box <span>All the convenience of a countertop recipe box, minus the crumbs and splats.</span></h1>
-
-        <ul class="navbar">
-            <li><a href="./">HOME</li>
-            <li><a href="./ingredients">INGREDIENTS</a></li>
-            <li><a href="./recipes">RECIPES</a></li>
-        </ul>
-
-
-    </body>
-</html>
+@endsection
--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-        <h1>Recipes</h1>
+<h1>Add a Recipe</h1>
 
 @endsection
