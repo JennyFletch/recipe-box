1736964d84c257a38deeccbc39311ffac5f9accf
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-10-24 14:59:58 -0600

    Style of individual recipe page

--- resources/views/recipes/single.blade.php
+++ resources/views/recipes/single.blade.php
@@ -2,33 +2,41 @@
 
 @section('content')
 
-    <div class="singlerecipe-img">
-        <img src="{{ asset('/images/'.$singlerecipe->image_url) }}" alt="{{ $singlerecipe->title }}" />
-    </div>
-    <h1>{{ $singlerecipe->title }}</h1>
+    <div class="singlerecipe-outer">
 
-    <p>
-        @if( ($singlerecipe->category !== 'none of these') && ($singlerecipe->category !== '- select one -') )
-            <a href="/recipes/filtered/{{ $singlerecipe->category }}" class="cat-button">{{ $singlerecipe->category }}</a>
-        @endif
-        @if( ($singlerecipe->diet !== 'none of these') && ($singlerecipe->diet !== '- select one -') )
-            <a href="/recipes/filtered/{{ $singlerecipe->diet }}" class="cat-button">{{ $singlerecipe->diet }}</a>
-        @endif
-        @if( ($singlerecipe->tool !== 'none of these') && ($singlerecipe->tool !== '- select one -') )
-            <a href="/recipes/filtered/{{ $singlerecipe->tool }}" class="cat-button">{{ $singlerecipe->tool }}</a>
-        @endif
-    </p>
-
-    <h2>{{ $singlerecipe->short_description }}</h2>
-
-
-    <div class="ingred-list">
-        <h3>INGREDIENTS</h3>
-        <p>~ Do a join to get ingredients! ~</p>
-        <h3>INSTRUCTIONS</h3>
-        <div class="single-instructions">
-            {{ $singlerecipe->instructions }}
+        <div class="singlerecipe-img">
+            <img src="{{ asset('/images/'.$singlerecipe->image_url) }}" alt="{{ $singlerecipe->title }}" />
         </div>
+        <h1 class="singlerecipe-title">{{ $singlerecipe->title }}</h1>
+
+        <p class="smalltxt text-center">
+            @if( ($singlerecipe->category !== 'none of these') && ($singlerecipe->category !== '- select one -') )
+                <a href="/recipes/filtered/{{ $singlerecipe->category }}" class="cat-button">{{ $singlerecipe->category }}</a>
+            @endif
+            @if( ($singlerecipe->diet !== 'none of these') && ($singlerecipe->diet !== '- select one -') )
+                <a href="/recipes/filtered/{{ $singlerecipe->diet }}" class="cat-button">{{ $singlerecipe->diet }}</a>
+            @endif
+            @if( ($singlerecipe->tool !== 'none of these') && ($singlerecipe->tool !== '- select one -') )
+                <a href="/recipes/filtered/{{ $singlerecipe->tool }}" class="cat-button">{{ $singlerecipe->tool }}</a>
+            @endif
+        </p>
+
+        <h2 class="singlerecipe-shortdesc">{{ $singlerecipe->short_description }}</h2>
+
+
+        <div class="singlerecipe-body">
+            <div class="singlerecipe-ingred">
+                <h3>INGREDIENTS</h3>
+                <p>~ Do a join to get ingredients! ~</p>
+            </div>
+            <div class="singlerecipe-instr">
+                <h3>INSTRUCTIONS</h3>
+                <div class="single-instructions">
+                    {{ $singlerecipe->instructions }}
+                </div>
+            </div>
+        </div>
+
     </div>
             
 
